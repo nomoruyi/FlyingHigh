997df7fc3fbc10aaa0cee135ea82885c147388ef
Nosa <nosakhare_omoruyi@hotmail.de>
2023-06-18 19:55:20 +0200

    parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -6,7 +6,7 @@ public class Parallax : MonoBehaviour
 {
 
     private float length;
-    private float startposition;
+    private float currentSpritePositionX;
     public GameObject cam;
 
     [SerializeField][Range(0f, 1f)] float parallaxEffect;
@@ -14,23 +14,21 @@ public class Parallax : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startposition = transform.position.x;
+        currentSpritePositionX = transform.position.x;
         SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-        Debug.Log("Hurensohn: " + renderer.gameObject.name)
-        ;
         length = renderer.bounds.size.x;
-
     }
 
     // Update is called once per frame
     void Update()
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        //Faktor, um den sich das Sprite in die entgegengesetzte Richtung bewegt 
         float dist = (cam.transform.position.x * -parallaxEffect);
 
-        transform.position = new Vector3(startposition + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(currentSpritePositionX + dist, transform.position.y, transform.position.z);
 
-        if (temp > startposition + length) startposition += length;
-        else if (temp < startposition - length) startposition -= length;
+        if (temp > currentSpritePositionX + length) currentSpritePositionX += length;
+        else if (temp < currentSpritePositionX - length) currentSpritePositionX -= length;
     }
 }
